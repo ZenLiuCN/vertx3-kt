7d2fa7179d98a03bc11d030ce89698b2446e30dc
Zen.Liu <lcz20@163.com>
2020-04-02 00:10:26 +0800

    add hocon

--- app/src/main/kotlin/cn/zenliu/vertx/kotlin/WebApi.kt
+++ app/src/main/kotlin/cn/zenliu/vertx/kotlin/WebApi.kt
@@ -1,9 +1,11 @@
 package cn.zenliu.vertx.kotlin
 
-import io.vertx.core.*
+import io.vertx.config.*
 import io.vertx.core.http.*
+import io.vertx.core.json.*
 import io.vertx.core.logging.*
 import io.vertx.ext.web.*
+import io.vertx.kotlin.core.json.*
 import io.vertx.kotlin.coroutines.*
 import java.time.*
 
@@ -14,21 +16,42 @@ class WebApi : CoroutineVerticle() {
 	private val router: Router by lazy { Router.router(vertx) }
 	private val server: HttpServer by lazy { vertx.createHttpServer() }
 	private val logger: Logger by lazy { LoggerFactory.getLogger(this::class.java) }
-	private val port: Int get() = config.getInteger("http.port", 8080)
+	private lateinit var conf: JsonObject
+	private val port get() = conf.get<JsonObject>("http").getInteger("port", 8080)
+	private val retriever by lazy {
+		ConfigRetriever.create(vertx,
+			ConfigRetrieverOptions()
+				.addStore(
+					ConfigStoreOptions(
+						json {
+							obj(
+								"type" to "file",
+								"format" to "hocon",
+								"config" to obj {
+									put("path", "app.conf")
+								}
+							)
+
+						}
+					)))
+	}
+
 
 	override suspend fun start() {
-
-		register()
-		server.requestHandler(router).listen(port)
-		logger.info("service listen on $port")
-
+		retriever.getConfig {
+			conf = it.result()
+			register()
+			server.requestHandler(router).listen(port)
+			logger.info("service listen on $port")
+		}
 	}
 
 	override suspend fun stop() {
 		server.close()
+		logger.info("service listen on $port")
 	}
 
-	private suspend fun register() {
+	private fun register() {
 		router.get("/hello")
 			.handler { ctx ->
 				ctx.response().end("hello${Instant.now().atOffset(ZoneOffset.of("+8"))}")
