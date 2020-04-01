e5f733b6330f9f4931e0cdaa9f2c1b585490a0aa
Zen.Liu <lcz20@163.com>
2020-04-01 23:45:42 +0800

    add launcher

--- app/src/main/kotlin/cn/zenliu/vertx/kotlin/WebApi.kt
+++ app/src/main/kotlin/cn/zenliu/vertx/kotlin/WebApi.kt
@@ -1,19 +1,27 @@
 package cn.zenliu.vertx.kotlin
 
+import io.vertx.core.*
 import io.vertx.core.http.*
+import io.vertx.core.logging.*
 import io.vertx.ext.web.*
 import io.vertx.kotlin.coroutines.*
+import java.time.*
 
 /**
  *
  */
 class WebApi : CoroutineVerticle() {
-	val router: Router by lazy { Router.router(vertx) }
-	val server: HttpServer by lazy { vertx.createHttpServer() }
+	private val router: Router by lazy { Router.router(vertx) }
+	private val server: HttpServer by lazy { vertx.createHttpServer() }
+	private val logger: Logger by lazy { LoggerFactory.getLogger(this::class.java) }
+	private val port: Int get() = config.getInteger("http.port", 8080)
 
 	override suspend fun start() {
+
 		register()
-		server.requestHandler(router).listen(8080)
+		server.requestHandler(router).listen(port)
+		logger.info("service listen on $port")
+
 	}
 
 	override suspend fun stop() {
@@ -23,7 +31,7 @@ class WebApi : CoroutineVerticle() {
 	private suspend fun register() {
 		router.get("/hello")
 			.handler { ctx ->
-				ctx.response().end("hello")
+				ctx.response().end("hello${Instant.now().atOffset(ZoneOffset.of("+8"))}")
 			}
 	}
 }
